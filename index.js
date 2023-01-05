6e030d210903ad98678845b66bd184c0b635e75e
Eder Campos Ribeiro <edercribeiro@gmail.com>
2023-01-05 17:10:46 -0300

    Change the Title to Display a Winner

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ function selectPic(dice) {
     return "images/dice" + dice + ".png";;
 }
 
+function verifyWinner(number1, number2){
+    // 0 = Draw
+    // 1 = number1 wins!
+    // 2 = number0 wins!
+    if (number1 > number2) {
+        return "Player 1 Wins!"
+    } else if (number1 < number2) {
+        return "Player 2 Wins!"
+    } else {
+        return "Draw!";
+    }
+}
+
 randomNumber1 = dice();
 randomNumber2 = dice();
 pic1 = selectPic(randomNumber1);
@@ -18,3 +31,5 @@ pic2 = selectPic(randomNumber2);
 
 document.querySelector("img.img1").setAttribute("src",pic1);
 document.querySelector("img.img2").setAttribute("src",pic2);
+
+document.querySelector("h1").textContent = verifyWinner(randomNumber1, randomNumber2);
